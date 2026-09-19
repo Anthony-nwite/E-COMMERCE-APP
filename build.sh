31fc9072bb0ea4ce9c45a5eb4a94abcc2218228e
Anthony-nwite <anthonynwite68@gmail.com>
2026-09-19 11:25:02 +0100

    requirements

--- build.sh
+++ build.sh
@@ -2,7 +2,10 @@
 # exit on error
 set -o errexit
 
-pip install -r requirments.txt
+pip install -r requirements.txt
+
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+#postgresql://ecommerce_3qkp_user:i2gjHuZned1s1QXwUDo8q6blKZt426vu@dpg-dan5ombm8hqs73a5afi0-a/ecommerce_3qkp
